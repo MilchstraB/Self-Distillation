e14a0544b4abf2d31224b34dda99cf5b2ef06cc9
MilchstraB <523169432@qq.com>
2026-02-02 23:56:37 +0800

    train script

---
+++ scripts/train.sh
@@ -0,0 +1,34 @@
+export HF_HOME="<your data cache path>"
+
+deepspeed main.py \
+    --deepspeed ./scripts/zero2.json \
+    --use_vllm True \
+    --vllm_mode "colocate" \
+    --vllm_tensor_parallel_size 1 \
+    --vllm_gpu_memory_utilization 0.3 \
+    --vllm_enable_sleep_mode True \
+    --vllm_importance_sampling_correction True \
+    --model_name_or_path Qwen/Qwen2.5-7B-Instruct \
+    --output_dir <output_path> \
+    --learning_rate 2e-5 \
+    --num_train_epochs 1 \
+    --seed 42 \
+    --train_path data/tooluse_data/train_data.json \
+    --eval_path data/tooluse_data/eval_data.json \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --warmup_ratio 0.1 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --bf16 True \
+    --fp16 False \
+    --max_prompt_length 1024 \
+    --max_completion_length 1024 \
+    --save_steps 100 \
+    --max_grad_norm 1 \
+    --report_to "wandb" \
+    --log_completions False \
+    --sync_ref_model True \
+    --ref_model_sync_steps 1 \
+    --ref_model_mixup_alpha 0.01 \
+    --num_loss_tokens_to_skip 3
