1fa3269fd559cd4b17767389c0cad2002de79b60
MilchstraB <523169432@qq.com>
2026-02-04 18:09:56 +0800

    bash

--- scripts/train.sh
+++ scripts/train.sh
@@ -16,6 +16,11 @@ export HF_HOME="<your data cache path>"
 # =================== Script Execution ===================
 # You shouldn't need to modify anything below this line
 # ========================================================
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+SAVE_DIR="${OUTPUT_DIR}/${EXP_NAME}/${TIMESTAMP}"
+
+mkdir -p "${SAVE_DIR}"
+
 deepspeed main.py \
     --deepspeed ./scripts/zero2.json \
     --use_vllm True \
@@ -24,13 +29,13 @@ deepspeed main.py \
     --vllm_gpu_memory_utilization 0.3 \
     --vllm_enable_sleep_mode True \
     --vllm_importance_sampling_correction True \
-    --model_name_or_path $MODEL_PATH \
-    --output_dir $OUTPUT_DIR \
+    --model_name_or_path "${MODEL_PATH}" \
+    --output_dir "${SAVE_DIR}" \
     --learning_rate 2e-5 \
     --num_train_epochs 1 \
     --seed 42 \
-    --train_path $TRAIN_DATA \
-    --eval_path $VAL_DATA \
+    --train_path "${TRAIN_DATA}" \
+    --eval_path "${VAL_DATA}" \
     --per_device_train_batch_size 4 \
     --gradient_accumulation_steps 8 \
     --warmup_ratio 0.1 \
@@ -41,6 +46,8 @@ deepspeed main.py \
     --max_prompt_length 1024 \
     --max_completion_length 1024 \
     --save_steps 100 \
+    --save_total_limit 1 \
+    --save_only_model True \
     --max_grad_norm 1 \
     --report_to "wandb" \
     --log_completions False \
@@ -48,5 +55,5 @@ deepspeed main.py \
     --ref_model_sync_steps 1 \
     --ref_model_mixup_alpha 0.01 \
     --num_loss_tokens_to_skip 3 \
-    --project $PROJECT_NAME \
-    --run_name $EXP_NAME
+    --project "${PROJECT_NAME}" \
+    --run_name "${EXP_NAME}" > >(tee -a "${SAVE_DIR}/train.log") 2>&1
